1875d5cde9753fe1e191fa0b76d8770eff22781e
jaysn1 <jay.nankani1@gmail.com>
2020-10-27 20:11:24 -0700

    data preprocessing

--- reading_data.py
+++ reading_data.py
@@ -25,17 +25,27 @@ def reading_files(base_directory = "train"):
 
     print("Total CUIs: ", len(CUI))
 
+    combined_texts_filename = "{}/{}.txt".format(base_directory, "combined_text")
+    transformed_combined_texts_filename = "{}/{}.txt".format(base_directory, "transformed_combined_text")
+
     # reading norm and note files from list of files
     data = []
+    all_notes = []
+    all_transformed_notes = []
     for _, filename in enumerate(files):
         norm_filename = "{}/train_norm/{}.norm".format(base_directory, filename)
         note_filename = "{}/train_note/{}.txt".format(base_directory, filename)
+        transformed_texts_filename = "{}/train_note/{}.txt".format(base_directory, "transformed_"+filename)
 
         with open(norm_filename) as f1:
             with open(note_filename) as f2:
                 data.append({'name':filename, 
                              'norm':list(map(lambda x: x.strip().split("||"), f1.readlines())), 
                              'note':" ".join(list(map(lambda x: x.strip(), f2.readlines())))    })
+        
+        all_notes.append(data[-1]['note'])
+        transformed_note = data[-1]['note']
+        st, en = 0, 0
         for x in data[-1]['norm']:
             if len(x)==4:
                 (i, cui, st, en) = x
@@ -50,18 +60,30 @@ def reading_files(base_directory = "train"):
                 CUI[cui].add((_, "|".join(mentions)))
             else:
                 raise ValueError("{} is wrong".format(x))
+            transformed_note = transformed_note[:int(st)] + cui + transformed_note[int(en):]
+        with open(transformed_texts_filename, 'w') as f3:
+            f3.write(transformed_note)
+        all_transformed_notes.append(transformed_note)
 
-    print("Total Data: ", len(data))
+
+    with open(combined_texts_filename, 'w') as f4:
+        f4.write('\n'.join(all_notes))
+    with open(transformed_combined_texts_filename, 'w') as f5:
+        f5.writelines('\n'.join(all_transformed_notes))
+
+
+
+    # print("Total Data: ", len(data))
 
     return data, CUI
 
 
 if __name__ == "__main__":
-    base_dir = "C:/Users/monil/Desktop/BMI 598 - NLP/Project/Clinical-Entity-Normalization/train"
+    base_dir = "C:/Users/Jaysn/Anaconda3/envs/NLP/Clinical-Entity-Normalization/train"
     data, CUI = reading_files(base_dir)
 
-    cui = "C0333307"
-    print("For CUI: ", cui)
-    print(CUI[cui])
-    for _, mention in CUI[cui]:
-        print(mention, " : ", data[_]['note'][:100], end="\n\n")
+    # cui = "C0333307"
+    # print("For CUI: ", cui)
+    # print(CUI[cui])
+    # for _, mention in CUI[cui]:
+    #     print(mention, " : ", data[_]['note'][:100], end="\n\n")
